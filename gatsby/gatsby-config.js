8eb17815e813e6bc037d6e466c2957ce3d06ce51
edisonthk <edisonthk@gmail.com>
2020-07-13 17:28:31 +0900

    update config

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `My Title`,
+    description: `My description`,
     author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
